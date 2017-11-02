289bdb9513aeca73f144ce65416867cb78a2b4e3
Daniel Pareja <parejadan@gmail.com>
2017-11-02 19:26:48 -0400

    refactored config setup and database initialization

--- feature/__init__.py
+++ feature/__init__.py
@@ -1,6 +1,5 @@
-import json
 from flask import Flask, render_template
-from feature.model import db, Client, ClientFeatureRequest
+from feature.config import ConfigManager
 
 # from v1.clients import client_api
 # from v1.features import feature_api
@@ -8,44 +7,6 @@ from feature.api.v1.clients import client_api
 from feature.api.v1.features import feature_api
 from feature.api.v1.priorities import priority_api
 from feature.api.v1.products import product_api
-
-
-class ConfigManager:
-    def __init__(self, file='config.json'):
-        """load application configurations from json - alternative to env variables"""
-        self._config_cache = self._get_config(file)
-        self._user = self._config_cache['user']
-        self._password = self._config_cache['password']
-        self._host = self._config_cache['host']
-        self._port = self._config_cache['port']
-        self._db = self._config_cache['db']
-        self._database_uri = self._config_cache['SQLALCHEMY_DATABASE_URI']
-        self._track_modifications = self._config_cache['SQLALCHEMY_TRACK_MODIFICATIONS']
-        self.setup_database = self._config_cache['SETUP_DATABASE']
-
-    @staticmethod
-    def _get_config(file):
-        try:
-            with open(file) as json_data_file:
-                return json.load(json_data_file)
-        except IOError as ex:
-            raise IOError("config file was not setup correctly: {}".format(ex))
-
-    def _get_database_uri(self):
-        database_uri = self._database_uri.format(self._user, self._password, self._host, self._port, self._db)
-        # print(database_uri)
-        return database_uri
-
-    def apply_config(self, _app):
-        _app.config['SQLALCHEMY_DATABASE_URI'] = self._get_database_uri()
-        _app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = self._track_modifications
-
-    def app_run(self, _app):
-        db.init_app(app)
-        # build database if environment config has it set
-        if self.setup_database:
-            with app.app_context():
-                db.create_all()
 
 
 app, env_config = Flask(__name__), ConfigManager()
---
+++ feature/config.py
@@ -0,0 +1,44 @@
+import json
+from feature.model import db
+
+
+class ConfigManager:
+    def __init__(self, file='config.json'):
+        """load application configurations from json - alternative to env variables"""
+        self._config_cache = self._get_config(file)
+        self._user = self._config_cache['user']
+        self._password = self._config_cache['password']
+        self._host = self._config_cache['host']
+        self._port = self._config_cache['port']
+        self._db = self._config_cache['db']
+        self._database_uri = self._config_cache['SQLALCHEMY_DATABASE_URI']
+        self._track_modifications = self._config_cache['SQLALCHEMY_TRACK_MODIFICATIONS']
+        self.setup_database = self._config_cache['SETUP_DATABASE']
+
+    @staticmethod
+    def _get_config(file):
+        try:
+            with open(file) as json_data_file:
+                return json.load(json_data_file)
+        except IOError as ex:
+            raise IOError("config file was not setup correctly: {}".format(ex))
+
+    def _get_database_uri(self):
+        database_uri = self._database_uri.format(self._user, self._password, self._host, self._port, self._db)
+        # print(database_uri)
+        return database_uri
+
+    def apply_config(self, _app):
+        _app.config['SQLALCHEMY_DATABASE_URI'] = self._get_database_uri()
+        _app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = self._track_modifications
+
+    @staticmethod
+    def setup_database(_app):
+        with _app.app.context():
+            db.create_all()
+
+    def app_run(self, _app):
+        db.init_app(_app)
+        # build database if environment config has it set
+        if self.setup_database:
+            self.setup_database(_app)
